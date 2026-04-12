728438b5498937d55e9573f59c57bb3bbbd3488f
Keylor Calderón Vega <43432543+KeylorCalderon@users.noreply.github.com>
2026-04-12 17:24:07 -0600

    Optimization for the Galaxy component

--- src/App.jsx
+++ src/App.jsx
@@ -64,18 +64,7 @@ function App() {
         }
       }}
     >
-      <Galaxy
-        density={1}
-        glowIntensity={0.3}
-        saturation={0}
-        hueShift={140}
-        twinkleIntensity={0.3}
-        rotationSpeed={0.1}
-        repulsionStrength={2}
-        autoCenterRepulsion={0}
-        starSpeed={0.5}
-        speed={1}
-      />
+      <Galaxy speed={1.0} density={1.0} glowIntensity={0.3} saturation={0.0} />
       <div className="ui">
         <GlitchText>THE FINAL QUESTION</GlitchText>
         <div className="history">
